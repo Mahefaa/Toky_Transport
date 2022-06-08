5e9639093bdf41bcc3c0e72427b89b52c937e9c0
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-06-08 23:29:42 +0300

    feat : labels for a city

---
+++ 31.sql
@@ -0,0 +1,1 @@
+select offre.id_offre, tarif, label from offre inner join voyage on offre.id_offre = voyage.id_offre  inner join ville on ville.id_ville = voyage.id_ville_depart where nom_ville ='Antananarivo' group by offre.id_offre;
