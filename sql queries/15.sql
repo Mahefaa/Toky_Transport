d9599735daba7920bb8a2b7b3baf685856e3c706
Steven1805 <99425144+Steven1805@users.noreply.github.com>
2022-06-16 16:12:52 +0300

    feat:Moved sql into a folder

--- sql queries/15.sql
+++ sql queries/15.sql
@@ -1,2 +1,2 @@
 select ville.nom_ville, offre.label, offre.tarif from voyage join ville on voyage.id_ville_aller = ville.id_ville
-join offre on offre.id_voyage = voyage.id_voyage;
+join offre on offre.id_voyage = voyage.id_voyage; 
