812c16d081e3c830ccdd97ca257530f877bb2710
Steven1805 <99425144+Steven1805@users.noreply.github.com>
2022-06-22 21:36:06 +0300

    Update 57.sql

--- sql queries/57.sql
+++ sql queries/57.sql
@@ -1,2 +1,4 @@
+--liste des depenses de chaque vehicule
+--Difficulté : Easy // Utilité : 6
 select vehicule.matricule, sum(depense.essence + depense.maintenance) from vehicule join depense on vehicule.id_depense = depense.id_depense
 group by vehicule.matricule;
