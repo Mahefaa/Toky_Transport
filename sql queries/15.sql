f7c51e10c0199a0e0aaf3eec6c60a3799496baa5
Steven1805 <99425144+Steven1805@users.noreply.github.com>
2022-06-22 21:30:43 +0300

    Update 15.sql

--- sql queries/15.sql
+++ sql queries/15.sql
@@ -1,2 +1,4 @@
+--afficher le nom des villes de destinations, les offres liés avec avec leur tarif.
+--Difficulté : Medium // Utilité : 10
 select ville.nom_ville, offre.label, offre.tarif from voyage join ville on voyage.id_ville_aller = ville.id_ville
 join offre on offre.id_voyage = voyage.id_voyage; 
