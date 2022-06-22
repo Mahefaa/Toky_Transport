29a14c507b8508e250cb14e23c2a071c57fd7aa9
Steven1805 <99425144+Steven1805@users.noreply.github.com>
2022-06-22 21:34:03 +0300

    Update 37.sql

--- sql queries/37.sql
+++ sql queries/37.sql
@@ -1,2 +1,4 @@
+--Le nombre de voyage dans une ville entre deux date precise
+--Dificulté : Easy // Utilité : 8
 select count(id_voyage) from voyage join ville on voyage.id_ville_aller = ville.id_ville 
 where voyage.id_ville_aller = parameter and voyage.date_voyage > parameter and voyage.date_voyage < parameter;
